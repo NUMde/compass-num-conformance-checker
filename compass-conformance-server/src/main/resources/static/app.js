dc94df0c61fe811f9a87a4d24326fa193594c406
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2021-09-16 16:46:00 +0200

    added second button to allow gecco conformity check

--- compass-conformance-server/src/main/resources/static/app.js
+++ compass-conformance-server/src/main/resources/static/app.js
@@ -1,28 +1,68 @@
 $(document).ready(function () {
     $("#resource-form").submit(validateResource);
+
+    $("form button[type=submit]").click(function() {
+        $("button[type=submit]", $(this).parents("form")).removeAttr("clicked");
+        $(this).attr("clicked", "true");
+    });
 });
 
 function validateResource(event) {
     event.preventDefault();
-
-    // clear & lock UI
+    // clear 
     $("#messages").empty();
-    lockUI(true);
+    var val = $("button[type=submit][clicked=true]").val();
+    if(val==="validate"){
 
-    let url = $(this).attr("action");
-    let data = $("#resource-form textarea").val();
-    let contentType = data.startsWith("<") ? "application/xml" : "application/json"
-    return $.ajax({
-        type: "POST",
-        url: url,
-        headers: {
-            Accept: "application/json, text/plain; charset=utf-8",
-            "Content-Type": contentType
-        },
-        data: data,
-        success: showValidationMessages,
-        error: handleError
-    });
+        // lock UI
+        lockUI(true,"validate");
+
+        let url = "/fhir/$validate-single"
+        let data = $("#resource-form textarea").val();
+        let contentType = data.startsWith("<") ? "application/xml" : "application/json"
+        return $.ajax({
+            type: "POST",
+            url: url,
+            headers: {
+                Accept: "application/json, application/pdf, text/plain; charset=utf-8",
+                "Content-Type": contentType
+            },
+            data: data,
+            success: showValidationMessages,
+            error: handleError
+        });
+    } else{
+        lockUI(true,"gecco");
+
+        let url = "/fhir/$check-gecco-conformance";
+        let data = $("#resource-form textarea").val();
+        let contentType = data.startsWith("<") ? "application/xml" : "application/json"
+        return $.ajax({
+            type: "POST",
+            url: url,
+            headers: {
+                Accept: "application/json, application/pdf, text/plain; charset=utf-8",
+                "Content-Type": contentType
+            },
+            data: data,
+            success: function(blob,status,xhr){
+                var ct = xhr.getResponseHeader("content-type") || "";
+                    if (ct.indexOf('json') > -1) {
+                        showValidationMessages(blob);
+                    } 
+                    if(ct.indexOf('pdf') > -1){
+                        lockUI(false);
+                        var link=document.createElement('a');
+                        link.href=window.URL.createObjectURL(blob);
+                        link.download="certificate.pdf";
+                        link.click();
+
+                    }
+            },
+            error: handleError
+        });
+    }
+
 }
 
 function showValidationMessages(operationOutcome) {
@@ -45,14 +85,27 @@ function showValidationMessages(operationOutcome) {
     });
 }
 
+
+
 function handleError(request) {
     lockUI(false);
     addMessage("danger", `<b>HTTP ${request.status}</b> <pre>${request.responseText}</pre>`)
 }
 
-function lockUI(locked) {
-    $("#resource-form button, #resource-form textarea").prop("disabled", locked)
-    $(".spinner-border").toggleClass("hidden-spinner")
+function lockUI(locked, action="") {
+    if(locked === false){
+        $("#resource-form button, #resource-form textarea").prop("disabled", locked)
+        if(!$("#validate-spinner").hasClass("hidden-spinner")){
+            $("#validate-spinner").addClass("hidden-spinner");
+        } 
+        else if (!$("#gecco-spinner").hasClass("hidden-spinner")){
+            $("#gecco-spinner").addClass("hidden-spinner");
+        }
+    } else {
+        $("#resource-form button, #resource-form textarea").prop("disabled", locked)
+        $("#" + action + "-spinner").toggleClass("hidden-spinner")
+    }
+    
 }
 
 function addMessage(severity, message) {
